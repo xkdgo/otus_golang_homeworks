c2f9db868b90188d0751967724aa5768289ee976
xkdgo <xkdgo@rambler.ru>
2021-09-02 11:02:10 +0500

    HW2 Completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,73 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
+const backslash = rune('\u005c')
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func resetFlag(flag *bool) {
+	*flag = false
+}
+
+func setFlag(flag *bool) {
+	*flag = true
+}
+
+func deleteLastRune(b *strings.Builder) {
+	tmpString := b.String()
+	tmpRuneSlice := []rune(tmpString)
+	tmpRuneSlice = tmpRuneSlice[:len(tmpRuneSlice)-1]
+	b.Reset()
+	b.WriteString(string(tmpRuneSlice))
+}
+
+func Unpack(packedStr string) (unpacked string, err error) {
+	var buffered rune
+	var backslashedFlag bool
+	var multiplieredFlag bool
+	var b strings.Builder
+	for index, currentRune := range packedStr {
+		switch {
+		case index == 0 && unicode.IsDigit(currentRune):
+			b.Reset()
+			err = ErrInvalidString
+			return
+		case currentRune == backslash && !backslashedFlag:
+			setFlag(&backslashedFlag)
+			resetFlag(&multiplieredFlag)
+		case unicode.IsDigit(currentRune) && !backslashedFlag:
+			if multiplieredFlag {
+				b.Reset()
+				err = ErrInvalidString
+				return
+			}
+			multiplier, notOk := strconv.Atoi(string(currentRune))
+			if notOk != nil {
+				panic(notOk)
+			}
+			switch multiplier {
+			case 0:
+				deleteLastRune(&b)
+			case 1:
+				// do not need multiply
+			default:
+				b.WriteString(strings.Repeat(string(buffered), multiplier-1))
+			}
+			buffered = currentRune
+			resetFlag(&backslashedFlag)
+			setFlag(&multiplieredFlag)
+		default:
+			b.WriteRune(currentRune)
+			buffered = currentRune
+			resetFlag(&backslashedFlag)
+			resetFlag(&multiplieredFlag)
+		}
+	}
+	unpacked = b.String()
+	return
 }
