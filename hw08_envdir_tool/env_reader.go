f5983ab10fd62aa4e27e3050af81373bf0c22981
xkdgo <xkdgo@rambler.ru>
2021-11-29 17:24:44 +0500

    envReader without 0x00

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +21,59 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	envMap := make(map[string]EnvValue, 0)
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range files {
+		err = checkFileName(f.Name())
+		if err != nil {
+			log.Println("checkFileName ", err)
+			return nil, err
+		}
+		if !f.IsDir() {
+			pathToEnvFile := path.Join(dir, f.Name())
+			fileInfo, err := f.Info()
+			if err != nil {
+				log.Println("fileInfo ", err)
+				return nil, err
+			}
+			if fileInfo.Size() == 0 {
+				envMap[f.Name()] = EnvValue{"", true}
+				continue
+			}
+			content, err := processFileContent(pathToEnvFile)
+			if err != nil {
+				log.Println("processFileContent", err)
+				return nil, err
+			}
+			envMap[f.Name()] = EnvValue{content, false}
+		}
+
+	}
+	return envMap, nil
+}
+
+func checkFileName(nameOfFile string) error {
+	fmt.Println("checkFileName will implement later")
+	return nil
+}
+
+func processFileContent(pathToEnvFile string) (content string, err error) {
+	fd, err := os.Open(pathToEnvFile)
+	if err != nil {
+		log.Println("Open ", err)
+		return "", err
+	}
+	bufReader := bufio.NewReader(fd)
+	// TODO replace ReadString with ReadSlice
+	unprocessedEnvVar, err := bufReader.ReadString(byte('\n'))
+	if err != nil && err != io.EOF {
+		log.Println("bufReader", err)
+		return "", err
+	}
+	trimmedEnVar := strings.TrimRight(unprocessedEnvVar, " \t\n")
+
+	return trimmedEnVar, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,48 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+// func TestReadDir(t *testing.T) {
+// 	// Place your code here
+// }
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	Env := make(map[string]EnvValue, 0)
+	Env["HELLO"] = EnvValue{Value: "\"hello\"", NeedRemove: false}
+	Env["BAR"] = EnvValue{Value: "bar", NeedRemove: false}
+	Env["FOO"] = EnvValue{Value: "   foo\nwith new line", NeedRemove: false}
+	Env["EMPTY"] = EnvValue{Value: "", NeedRemove: false}
+	Env["UNSET"] = EnvValue{Value: "", NeedRemove: true}
+
+	type args struct {
+		dir string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Environment
+		wantErr bool
+	}{
+		{name: "test directory env",
+			args:    args{dir: "testdata/env"},
+			want:    Env,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := ReadDir(tt.args.dir)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadDir() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReadDir() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
