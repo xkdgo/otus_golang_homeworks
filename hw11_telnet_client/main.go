3b5e3344c813184ff23b27f1c7d57844cc04214b
xkdgo <xkdgo@rambler.ru>
2021-12-06 18:06:57 +0500

    Telnet client init commit without own tests

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,51 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var userTimeout string
+	var address string
+	var wg sync.WaitGroup
+	flag.StringVar(&userTimeout, "timeout", "10s", "timeout of telnet operation")
+	flag.Parse()
+	args := flag.Args()
+	switch len(args) {
+	case 1:
+		address = fmt.Sprintf("%s:", args[0])
+	case 2:
+		address = fmt.Sprintf("%s:%s", args[0], args[1])
+	default:
+		log.Fatalf("You should enter \"address port\"")
+	}
+	timeout, err := time.ParseDuration(userTimeout)
+	if err != nil {
+		log.Fatalf("You should enter timeout like \"10s\"")
+	}
+
+	telnetClient := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+	err = telnetClient.Connect()
+	if err != nil {
+		log.Fatalf("Could not connect to server %s", address)
+	}
+	wg.Add(2)
+	go func(wg *sync.WaitGroup, client TelnetClient) {
+		defer client.Close()
+		defer wg.Done()
+		telnetClient.Send()
+	}(&wg, telnetClient)
+	go func(wg *sync.WaitGroup, client TelnetClient) {
+		defer client.Close()
+		defer wg.Done()
+		telnetClient.Receive()
+	}(&wg, telnetClient)
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -13,9 +18,75 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	return &TelnetConnection{
+		Address: address,
+		Ctx:     ctx,
+		Cancel:  cancel,
+		In:      in,
+		Out:     out,
+	}
 }
 
 // Place your code here.
 // P.S. Author's solution takes no more than 50 lines.
+type TelnetConnection struct {
+	Address    string
+	Ctx        context.Context
+	Cancel     context.CancelFunc
+	In         io.ReadCloser
+	Out        io.Writer
+	Connection net.Conn
+}
+
+func (t *TelnetConnection) Connect() error {
+	dialer := &net.Dialer{}
+	conn, err := dialer.DialContext(t.Ctx, "tcp", t.Address)
+	t.Connection = conn
+	return err
+}
+
+func (t *TelnetConnection) Close() error {
+	t.Cancel()
+	return t.Connection.Close()
+}
+
+func (t *TelnetConnection) Send() error {
+	scanner := bufio.NewScanner(t.In)
+OUTER:
+	for {
+		select {
+		case <-t.Ctx.Done():
+			break OUTER
+		default:
+			if !scanner.Scan() {
+				break OUTER
+			}
+			str := scanner.Text()
+			t.Connection.Write([]byte(fmt.Sprintf("%s\n", str)))
+		}
+	}
+	log.Printf("Finished writeRoutine")
+	return nil
+}
+
+func (t *TelnetConnection) Receive() error {
+	scanner := bufio.NewScanner(t.Connection)
+OUTER:
+	for {
+		select {
+		case <-t.Ctx.Done():
+			break OUTER
+		default:
+			if !scanner.Scan() {
+				log.Printf("Disconnected from remote server")
+				break OUTER
+			}
+			text := scanner.Text()
+			t.Out.Write([]byte(fmt.Sprintf("%s\n", text)))
+		}
+	}
+	log.Printf("Finished readRoutine")
+	return nil
+}
