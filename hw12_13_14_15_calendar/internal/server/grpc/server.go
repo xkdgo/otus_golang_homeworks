1a95198c0c1abde00cf5cda4cee0e150bef51955
xkdgo <xkdgo@rambler.ru>
2022-01-19 14:17:34 +0500

    realised GRPC API funcs for ListEvents

--- hw12_13_14_15_calendar/internal/server/grpc/server.go
+++ hw12_13_14_15_calendar/internal/server/grpc/server.go
@@ -10,12 +10,18 @@ import (
 
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/pkg/errors"
 	pb "github.com/xkdgo/otus_golang_homeworks/hw12_13_14_15_calendar/internal/server/grpc/proto"
 	"github.com/xkdgo/otus_golang_homeworks/hw12_13_14_15_calendar/internal/storage"
 	"google.golang.org/grpc"
+)
+
+const (
+	timelayout = "2006-01-02"
 )
 
 type Logger interface { // TODO
@@ -95,8 +101,12 @@ func (s *Service) CreateEvent(ctx context.Context, ev *pb.Event) (res *pb.Create
 	return &pb.CreateEventResponse{Id: id}, nil
 }
 
-func (s *Service) UpdateEvent(context.Context, *pb.Event) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateEvent not implemented")
+func (s *Service) UpdateEvent(ctx context.Context, ev *pb.Event) (empty *emptypb.Empty, err error) {
+	err = s.app.UpdateEvent(ctx, ev.Id, ev.Title, ev.UserID, ev.Datetimestart.AsTime(), ev.Duration.AsDuration(), ev.Alarmtime.AsTime())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	return &emptypb.Empty{}, nil
 }
 
 func (s *Service) DeleteEvent(ctx context.Context, req *pb.DeleteEventRequest) (res *emptypb.Empty, err error) {
@@ -107,14 +117,59 @@ func (s *Service) DeleteEvent(ctx context.Context, req *pb.DeleteEventRequest) (
 	return &emptypb.Empty{}, nil
 }
 
-func (s *Service) ListEventsDay(context.Context, *pb.ListEventsRequest) (*pb.Events, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ListEventsDay not implemented")
+func (s *Service) ListEventsDay(ctx context.Context, req *pb.ListEventsRequest) (res *pb.Events, err error) {
+	day := req.Datetimestart
+	dayTime, err := time.Parse(timelayout, day)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+	events, err := s.app.ListEventsDay(ctx, req.UserID, dayTime)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	protoEvents := convertToProtoEvents(events)
+	return &pb.Events{Events: protoEvents}, nil
 }
 
-func (s *Service) ListEventsWeek(context.Context, *pb.ListEventsRequest) (*pb.Events, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ListEventsWeek not implemented")
+func (s *Service) ListEventsWeek(ctx context.Context, req *pb.ListEventsRequest) (res *pb.Events, err error) {
+	day := req.Datetimestart
+	dayTime, err := time.Parse(timelayout, day)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+	events, err := s.app.ListEventsWeek(ctx, req.UserID, dayTime)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	protoEvents := convertToProtoEvents(events)
+	return &pb.Events{Events: protoEvents}, nil
 }
 
-func (s *Service) ListEventsMonth(context.Context, *pb.ListEventsRequest) (*pb.Events, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ListEventsMonth not implemented")
+func (s *Service) ListEventsMonth(ctx context.Context, req *pb.ListEventsRequest) (res *pb.Events, err error) {
+	day := req.Datetimestart
+	dayTime, err := time.Parse(timelayout, day)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+	events, err := s.app.ListEventsWeek(ctx, req.UserID, dayTime)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	protoEvents := convertToProtoEvents(events)
+	return &pb.Events{Events: protoEvents}, nil
+}
+
+func convertToProtoEvents(events []storage.Event) (protoEvents []*pb.Event) {
+	protoEvents = make([]*pb.Event, 0, len(events))
+	for _, event := range events {
+		protoEvents = append(protoEvents, &pb.Event{
+			Id:            event.ID,
+			Title:         event.Title,
+			UserID:        event.UserID,
+			Datetimestart: timestamppb.New(event.DateTimeStart),
+			Duration:      durationpb.New(event.Duration),
+			Alarmtime:     timestamppb.New(event.AlarmTime),
+		})
+	}
+	return protoEvents
 }
