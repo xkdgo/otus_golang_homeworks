349a139873e8bcc2c7588ab0640244d6abb7f510
xkdgo <xkdgo@rambler.ru>
2022-02-08 17:43:00 +0500

    added hard reconnects to storage when it inits

--- hw12_13_14_15_calendar/cmd/calendar_scheduler/service/runApp.go
+++ hw12_13_14_15_calendar/cmd/calendar_scheduler/service/runApp.go
@@ -36,8 +36,18 @@ func RunApp(config config.SchedulerConfig) {
 	storage, err := helper.InitStorage(config.Storage.Type, config.Storage.DSN)
 	if err != nil {
 		logg.Error("cant init storage:", errors.Wrapf(err, "%s", config.Storage.Type))
-		cancel()
-		os.Exit(1) //nolint:gocritic
+		for i := 0; i < 5; i++ {
+			time.Sleep(5 * time.Second)
+			storage, err = helper.InitStorage(config.Storage.Type, config.Storage.DSN)
+			if err == nil {
+				break
+			}
+		}
+		if err != nil {
+			logg.Error("cant init storage:", errors.Wrapf(err, "%s", config.Storage.Type))
+			cancel()
+			os.Exit(1) //nolint:gocritic
+		}
 	}
 
 	period, err := time.ParseDuration(config.Scheduler.TimeoutQuery)
